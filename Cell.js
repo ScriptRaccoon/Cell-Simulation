393bad155d61076f59e62ef4db2de033e86cc5cb
Script Raccoon <scriptraccoon@gmail.com>
2021-02-20 13:08:26 +0100

    adjust swim values

--- Cell.js
+++ Cell.js
@@ -1,8 +1,7 @@
-import { difference, rotate, randInt } from "./utils.js";
+import { difference, rotate, rand, randInt } from "./utils.js";
 import { Body } from "./Body.js";
 
 export const cells = [];
-
 export const cellLimit = 1000;
 
 export class Cell extends Body {
@@ -14,10 +13,11 @@ export class Cell extends Body {
         this.maxSpeed = randInt(300, 400);
         this.maxForce = randInt(20, 40);
         this.time = 0;
-        this.swim = { length: 50, amplitude: Math.PI / 6 };
-        const cellNumber = cells.length;
-        document.getElementById("info").innerText =
-            cellNumber == 1 ? `${cellNumber} Zelle` : `${cellNumber} Zellen`;
+        this.swim = {
+            length: randInt(40, 50),
+            amplitude: rand(Math.PI / 10, Math.PI / 8),
+        };
+        writeCellNumber();
     }
 
     update(food, deltaTime) {
@@ -29,4 +29,10 @@ export class Cell extends Body {
         this.applyForce(rotate(this.vel, swimAngle));
         this.updatePos(deltaTime);
     }
+}
+
+function writeCellNumber() {
+    const info = document.getElementById("info");
+    info.innerText = `${cells.length} Zelle`;
+    if (cells.length > 1) info.innerText += "n";
 }
--- utils.js
+++ utils.js
@@ -2,6 +2,10 @@ export function randInt(a, b) {
     return a + Math.floor((b - a) * Math.random());
 }
 
+export function rand(a, b) {
+    return a + (b - a) * Math.random();
+}
+
 export function distance(posA, posB) {
     return Math.sqrt(Math.pow(posA.x - posB.x, 2) + Math.pow(posA.y - posB.y, 2));
 }
