08ca1ce1e9f1f905a1999808ec21dc96ac46c569
Script Raccoon <scriptraccoon@gmail.com>
2021-11-19 22:35:19 +0100

    avoiding bugfix

--- public/js/features/targetClosest.js
+++ public/js/features/targetClosest.js
@@ -1,6 +1,7 @@
 import { difference } from "../utils.js";
 
 export const targetClosest = (type) => (body) => {
+    if (body.avoiding) return;
     body.target = body.getClosestOfType(type);
     if (body.target) {
         const targetForce = difference(body.pos, body.target.pos);
